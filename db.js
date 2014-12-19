b8352bfecc4e13780c204fe1f580088bc47cdd52
asallaup <arvid.sallaup@gmail.com>
2014-12-19 09:47:48 +0100

    Added heroku support

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'url' : '"mongodb://miles_user:miles_password@ds047030.mongolab.com:47030/heroku_app30746985"',
+  'url' : 'mongodb://miles_user:miles_password@ds047030.mongolab.com:47030/heroku_app30746985',
   'testurl' : 'mongodb://localhost/cvpartner_test'
 
 }
